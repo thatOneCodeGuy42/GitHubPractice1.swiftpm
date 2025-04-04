b88fdec3f94be87e62302a8bc2fb16b579c9b63a
George Koroulis <gkoroulis7201@stu.d214.org>
2025-04-04 11:05:21 -0500

    Added a frame around the names

--- ContentView.swift
+++ ContentView.swift
@@ -2,7 +2,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("George")
-        Text("Frank")
+        
+        ZStack {
+            
+            RoundedRectangle(cornerRadius: 15)
+                .foregroundStyle(Color.blue.opacity(0.3))
+                .frame(width: 300, height: 100)
+            VStack{
+                Text("George")
+                Text("Frank")
+            }
+        }
     }
 }
