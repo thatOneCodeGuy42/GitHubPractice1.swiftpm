c7fd035ddc479a95aa8ea0db83f1af880339c5fc
George Koroulis <gkoroulis7201@stu.d214.org>
2025-04-04 11:14:29 -0500

    Added an easy math question with the ability to check if it's correct or not.

--- ContentView.swift
+++ ContentView.swift
@@ -1,10 +1,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var answer = 0
+    @State var result = ""
     var body: some View {
         
         ZStack {
-            
             RoundedRectangle(cornerRadius: 15)
                 .foregroundStyle(Color.blue.opacity(0.3))
                 .frame(width: 300, height: 100)
@@ -13,5 +14,25 @@ struct ContentView: View {
                 Text("Frank")
             }
         }
+        HStack {
+            Text("3 + 3 = ")
+            TextField("Enter Answer Here", value: $answer, formatter: NumberFormatter())
+                .textFieldStyle(.roundedBorder)
+                .frame(width: 100)
+        }
+        Button("Check Answer") {
+            
+            if answer == 6 {
+                
+                result = "That's correct!"
+                
+            } else {
+                
+                result = "Sorry, that's incorrect. Please try again."
+                
+            }
+            
+        }
+        Text(result)
     }
 }
